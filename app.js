86a666f70e9834b4faf33cb83fd739f461650cf3
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-06-14 23:36:13 +0900

    #2.10 Recap

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-function plus(a, b) {
-    console.log(a + b);
-}
-
-function divide(a, b){
-    console.log(a/b);
-}
-
-divide(98, 20);
-plus(8, 60);
-
-const player = {
-    name: "gyuwon",
-    sayHello: function(otherPersonsName){
-        console.log("hello " + otherPersonsName + " nice to meet you!");
+const calculator = {
+    add: function(a, b){
+        console.log(a+b);
     },
-};
+    minus: function(a, b){
+        console.log(a-b);
+    },
+    multiple: function(a, b){
+        console.log(a*b);
+    },
+    divide: function(a, b){
+        console.log(a/b);
+    },
+    poworof: function(a, b){
+        console.log(a**b);
+    },
+}
 
-player.sayHello("lynn");
-player.sayHello("nico");
+calculator.add(10, 2);
+calculator.minus(10, 2);
+calculator.multiple(10, 2);
+calculator.divide(10, 2);
+calculator.poworof(10, 2);
