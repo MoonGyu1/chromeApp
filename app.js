5f57fa647cef4bfddd2e9df7971c2473336f688b
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-06-14 17:16:35 +0900

    introduction & data types 1

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "moon";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "gyuwon";
+
+console.log("your name is " + myName);
