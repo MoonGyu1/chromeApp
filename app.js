ca3bffde82f8285bd3603fadee1775837f44f25a
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-06-15 23:29:09 +0900

    #3.3 Events

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const age = parseInt(prompt("How old are you?"));
+const h1 = document.querySelector(".hello:first-child h1");
+console.dir(h1);
 
-if(isNaN(age) || age < 0){
-    console.log("Please write a real positive number");
-} else if (age < 18) {
-    console.log("You are too young");
-} else if(age >= 18 && age <= 50) {
-    console.log("you can drink");
-} else {
-    console.log("you can't drink");
-} 
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!"
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
